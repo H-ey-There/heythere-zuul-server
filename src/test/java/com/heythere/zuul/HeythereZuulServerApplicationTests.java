f42ee6e53946578b343fef39e0a3a35a53530616
Min1579 <hsm012362@gmail.com>
2020-09-16 19:06:03 +0900

    feat ⚡️ zuul gateway server init

---
+++ src/test/java/com/heythere/zuul/HeythereZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heythere.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeythereZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
