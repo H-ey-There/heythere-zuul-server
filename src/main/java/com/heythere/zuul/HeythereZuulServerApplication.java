6a9bd6f5bafbf181b86a6624479cf28597f75325
Min1579 <hsm012362@gmail.com>
2020-10-05 00:10:01 +0900

    feat ⚡️ add command line runner for default user

--- src/main/java/com/heythere/zuul/HeythereZuulServerApplication.java
+++ src/main/java/com/heythere/zuul/HeythereZuulServerApplication.java
@@ -1,18 +1,37 @@
 package com.heythere.zuul;
 
+import com.heythere.zuul.security.jwt.JwtProperties;
+import com.heythere.zuul.security.payload.SignUpRequest;
+import com.heythere.zuul.user.model.User;
+import com.heythere.zuul.user.repository.UserRepository;
+import com.heythere.zuul.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableJpaAuditing
 @SpringBootApplication
-public class HeythereZuulServerApplication {
+@EnableConfigurationProperties(JwtProperties.class)
+public class HeythereZuulServerApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HeythereZuulServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userService.registerUser(new SignUpRequest("admin", "admin@admin", "123"));
+	}
 }
--- src/main/java/com/heythere/zuul/security/SecurityConfiguration.java
+++ src/main/java/com/heythere/zuul/security/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,6 +64,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors()
@@ -81,6 +87,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(new RestAuthenticationEntryPoint())
                 .and()
                 .authorizeRequests()
+                .antMatchers("/swagger-ui/**","/v3/api-docs/**")
+                .permitAll()
                 .antMatchers("/api/community/**", "/community/**")
                 .permitAll()
                 .antMatchers("/",
@@ -93,8 +101,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/**/*.html",
                         "/**/*.css",
                         "/**/*.js",
-                        "/h2/**",
-                        "/swagger-ui/index.html/**")
+                        "/h2/**")
                 .permitAll()
                 .antMatchers("/auth/**", "/oauth2/**", "/greeting/**")
                 .permitAll()
