187800166a438f33fa261ea5ff72b15aeae4fb04
Min1579 <hsm012362@gmail.com>
2020-11-27 18:16:54 +0900

    feat ⚡ s3 bucket impl

---
+++ src/main/java/com/heythere/zuul/auth/service/AmazonS3StorageService.java
@@ -0,0 +1,84 @@
+package com.heythere.zuul.auth.service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.heythere.zuul.auth.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class AmazonS3StorageService {
+    private AmazonS3 amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+
+    @PostConstruct
+    public void setS3Client() {
+        final AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        amazonS3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+
+    public String upload(final MultipartFile file, final User user, final String dirName) throws IOException {
+        return upload(convert(file).get(), user, dirName);
+    }
+
+    private String upload(final File uploadFile, final User user, final String dirName) {
+        final String fileName = dirName + "/" + user.getId() + "-" + user.getEmail() + "-" + uploadFile.getName();
+        final String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    private String putS3(final File uploadFile, final String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(final File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private Optional<File> convert(final MultipartFile file) throws IOException {
+        final File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
+            try (final FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+}
+
